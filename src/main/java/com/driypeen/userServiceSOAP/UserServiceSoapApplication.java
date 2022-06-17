73b297b1e57d1f1ac1af7a51fb6160c0599f4bf8
driypeen <driypeen@gmail.com>
2022-06-17 11:09:03 +0400

    Initial commit

---
+++ src/main/java/com/driypeen/userServiceSOAP/UserServiceSoapApplication.java
@@ -0,0 +1,13 @@
+package com.driypeen.userServiceSOAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driypeen/userServiceSOAP/UserServiceSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driypeen.userServiceSOAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
